988aff9a03ecd2233848e298ce4fa5b75e07e6a4
William Gilbert Go <williamgilbertgo@gmail.com>
2017-07-06 08:28:26 +0000

    Added a new route for API search result

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var zomato = require("zomato");
 var router = express.Router();
 var restrict = require("../auth/restrict");
+var restaurantService = require("../services/restaurant-service"); 
 
 router.get('/', restrict, function(req, res, next) {
     var vm = { 
@@ -10,5 +12,15 @@ router.get('/', restrict, function(req, res, next) {
     }
     res.render('orders/index', vm);
 });  
+
+
+router.get('/api/restaurants', restrict, function(req, res, next) {
+    restaurantService.getRestaurants(function(err, restaurants) {
+        if(err) {
+            return res.status(500).json({error:'Failed to retrieve restaurants.'});
+        }
+        res.json(restaurants);
+    }); 
+}); 
 
 module.exports = router;
