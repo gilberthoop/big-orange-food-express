a7ba59c82173e64747cce46692e6d13da8b935ae
William Gilbert Go <williamgilbertgo@gmail.com>
2017-07-14 20:05:05 +0000

    Installed EatStreet API

--- config.js
+++ config.js
@@ -2,10 +2,5 @@ var config = {};
 
 config.mongoUri = 'mongodb://localhost:27017/rtr';
 config.cookieMaxAge = 30 * 24 * 3600 * 1000; 
-config.zomatorKey = '254204de027f7c3f65fca58fd218d4eb';
-config.coordinates = {
-    lat: 49.2827,
-    lon: -123.1207
-}
 
 module.exports = config;
