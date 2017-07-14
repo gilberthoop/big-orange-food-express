117747891c6baddba1f62deb33e71d875eb79356
William Gilbert Go <williamgilbertgo@gmail.com>
2017-07-14 20:08:33 +0000

    Fectching and displaying restaurant data

--- public/js/app/restaurants/restaurants.js
+++ public/js/app/restaurants/restaurants.js
@@ -5,26 +5,22 @@
         .module('app')
         .controller('RestaurantsController', RestaurantsController);
      
-    RestaurantsController.$inject = ['$http'];   
+    RestaurantsController.$inject = ['api'];   
     
-    function RestaurantsController($http) {
+    function RestaurantsController(api) {
         var self = this;
         
-        var USER_KEY = "254204de027f7c3f65fca58fd218d4eb";
-        var URL = "https://developers.zomato.com/api/v2.1/geocode?";
+        api.getRestaurants()
+            .then(function(data) {
+                self.restaurants = data;
+            }); 
         
-        //$http.get('/orders/api/restaurants')    
         
-        $http({
-                method: 'GET',
-                url: URL,
-                headers: {'user-key' : USER_KEY},
-                params: {
-                    lat: '49.2828', 
-                    lon: '-123.1067',
-                } 
-            })
-            .then(function(response) {     
+        
+        /* Before using factory
+        
+        $http.get('/orders/api/restaurants')
+            .then(function(response) {
                 self.restaurants = response.data; 
             },
             function(reason) {
@@ -32,6 +28,7 @@
             })
             .catch(function(err) {
                 console.log(err);
-            })
+            });
+        */
     } 
 }());
