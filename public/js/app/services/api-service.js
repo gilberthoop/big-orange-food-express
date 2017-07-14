6036c56d8a7faab798d2c1d21e0fef9ef6e622d9
William Gilbert Go <williamgilbertgo@gmail.com>
2017-07-14 20:07:33 +0000

    Set up factory service to fetch data from EatStreet API

--- public/js/app/services/api-service.js
+++ public/js/app/services/api-service.js
@@ -2,9 +2,6 @@
     
     'user strict';
     
-    var USER_KEY = "254204de027f7c3f65fca58fd218d4eb";
-    var URL = "https://developers.zomato.com/api/v2.1/geocode?";
-    
     angular
         .module('app')
         .factory('api', apiFactory);
@@ -13,24 +10,15 @@
     
     function apiFactory($http) {
         return {
-            getRestaurants: getRestaurants()
+            getRestaurants: getRestaurants
         };
         
         function getRestaurants() {
-            return $http({
-                method: 'GET',
-                url: URL,
-                headers: {'user-key' : USER_KEY},
-                params: {
-                    lat: '49.2828', 
-                    lon: '-123.1067',
-                } 
-            })
-            .then(function(response) {
-                return response.data;
-            });
+            return $http.get('/orders/api/restaurants')
+                .then(function(response) {
+                    return response.data;
+                });
         }
-        
     }
     
-})
+}());
