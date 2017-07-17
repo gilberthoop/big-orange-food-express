0ae4779b9599fa5b0a42126ba3cef0667b8cdd9a
William Gilbert Go <williamgilbertgo@gmail.com>
2017-07-17 00:08:28 +0000

    Created methods for placing order and check out

--- public/js/app/menu/menu.js
+++ public/js/app/menu/menu.js
@@ -6,15 +6,66 @@
         .module('app')
         .controller('MenuController', MenuController);
         
-    MenuController.$inject = ['api', '$routeParams'];
+    MenuController.$inject = ['api', '$routeParams', 'ngDialog', '$scope', '$location'];
     
-    function MenuController(api, $routeParams) {
+    function MenuController(api, $routeParams, ngDialog, $scope, $location) {
         var self = this;
+        
+        self.items = [];
         
         api.getRestaurantMenu($routeParams.restKey)
             .then(function(data) {
                 self.restaurant = data;
             });
+            
+            
+        self.viewItem = function(item) {
+            self.activeItem = item;
+            self.activeItem.options = [];
+            
+            ngDialog.open({
+                templateUrl: 'item.html',
+                className: 'ngdialog-theme-default',
+                scope: $scope
+            });
+        }
+        
+        self.toggleOption = function(option) {
+            var index = self.activeItem.options.indexOf(option);
+            
+            if(index > -1) {
+                self.activeItem.options.splice(index, 1);
+                return;
+            }
+            
+            self.activeItem.options.push(option);
+        };
+        
+        self.addItem = function(item) {
+            var newItem = {
+                id: item.id,
+                name: item.name,
+                price: item.price
+            };
+            
+            if(item.options.length > 0) {
+                newItem.options = item.options.map(function(item) {
+                    return {id: item.id, name: item.name, price: item.price};
+                });
+            }
+            
+            self.items.push(newItem);
+            ngDialog.close();
+            console.log(self.items);
+        }
+        
+        self.cancel = function() {
+            ngDialog.close();
+        }
+        
+        self.checkOut = function() {
+            $location.url('/payment');
+        }
     }
     
 }());
