866a4aa483fa9db2825ae9e2ad0025b34538b0c4
William Gilbert Go <williamgilbertgo@gmail.com>
2017-07-17 21:06:39 +0000

    Created a controller for payment

---
+++ public/js/app/payment/payment.js
@@ -0,0 +1,13 @@
+(function() {
+    
+    'user strict';
+    
+    angular
+        .module('app')
+        .controller('PaymentController', PaymentController);
+    
+    function PaymentController() {
+        
+    }
+    
+}());
