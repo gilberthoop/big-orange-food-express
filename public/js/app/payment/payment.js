5f6ea265afc2fa0b2d3ad3f1e6649a1eef9f2ec2
William Gilbert Go <williamgilbertgo@gmail.com>
2017-07-18 21:58:53 +0000

    Connected view and model using MVC pattern

--- public/js/app/payment/payment.js
+++ public/js/app/payment/payment.js
@@ -5,9 +5,23 @@
     angular
         .module('app')
         .controller('PaymentController', PaymentController);
-    
-    function PaymentController() {
         
+        PaymentController.$inject = ['api', '$location'];
+    
+    function PaymentController(api, $location) {
+        var self = this;
+        
+        self.submit = function() {
+            self.showProgress = true;
+            api.placeOrder(self.card)
+                .then(function(response) {
+                    if(response && response.success) {
+                        return $location.url('/confirmation');
+                    }
+                    alert('Something went wrong.');
+                });
+            return $location.url('/confirmation');
+        };
     }
     
 }());
